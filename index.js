fd56063aa24fc78cd40200e8fd9819e45b9541ba
Faith <faithbrianna01@gmail.com>
2023-12-18 18:27:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//saturdayFun takes the argument activity
+// roller-skate as default activity
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(task = 'go to the office'){
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+const wrapWithAsterisk = wrapAdjective('*');
+const wrapWithDoublePipe = wrapAdjective('||');
+
